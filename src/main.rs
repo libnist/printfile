d38fb90736a03fbadbd405191838411cfd64e9fc
libnist <ghahremani.mim@gmail.com>
2023-12-17 20:53:24 +0330

    takes path to files and prints them out.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::env;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    for arg in &args[1..]{
+        read_file(arg)
+    }
+}
+
+fn read_file(path: &str) {
+    let file = File::open(path);
+
+    match file {
+        // The happy path
+        Ok(file) => {
+            let reader = BufReader::new(file);
+            for line in reader.lines() {
+                println!("{}", line.unwrap());
+            }
+        },
+        // The unhappy path
+        Err(error) => {
+            match error.kind() {
+                std::io::ErrorKind::NotFound => {
+                    println!("There was no file at: {}", path)
+                },
+                _ => {
+                    println!("There is something wrong with the file at: {}", path)
+                }
+            }
+        },
+    };
 }
